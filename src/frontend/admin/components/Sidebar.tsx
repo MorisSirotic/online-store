7a756b1f32e6c5a9c427df3c8d5afe3a3265ebf8
Moris Sirotić <moris.sirotic1@gmail.com>
2023-04-29 17:40:27 +0200

    Added links for Sidebar

--- src/frontend/admin/components/Sidebar.tsx
+++ src/frontend/admin/components/Sidebar.tsx
@@ -1,15 +1,39 @@
-import { FaAddressBook } from "react-icons/fa";
+import { IconType } from "react-icons";
+import {
+  FaAddressCard,
+  FaBars,
+  FaBox,
+  FaComment,
+  FaDashcube,
+  FaShoppingCart,
+  FaStarOfLife,
+  FaUsers,
+} from "react-icons/fa";
 
-const links: Array<any> = ["Link", "Link", "Link", "Link"];
+
+const links: Array<{ name: string; url: string; icon: IconType }> = [
+  { name: "Dashboard", url: "/dashboard/", icon: FaDashcube },
+  { name: "Orders", url: "/dashboard/orders", icon: FaShoppingCart },
+  { name: "Products", url: "/dashboard/products", icon: FaBox },
+  { name: "Customers", url: "/dashboard/customers", icon: FaUsers },
+  { name: "Statistics", url: "/dashboard/statistics", icon: FaBars },
+  { name: "Reviews", url: "/dashboard/reviews", icon: FaComment },
+  { name: "Staff", url: "/dashboard/staff", icon: FaStarOfLife },
+  { name: "Hot Offers", url: "/dashboard/hot", icon: FaAddressCard },
+];
 
 export const Sidebar = () => {
   return (
     <div className="flex flex-col min-w-[200px] h-screen border-r-2  border-b-2  bg-white text-gray-500">
       {links.map((item, index) => {
         return (
-          <a key={index} className="flex py-2 px-4 text-lg hover:text-gray-400" href="#">
-            <FaAddressBook className=" self-center w-5 h-5 mr-2" />{" "}
-            {item + "#" + index}
+          <a
+            key={index}
+            className="flex py-2 px-4 text-lg hover:text-gray-400"
+            href={item.url}
+          >
+            <item.icon className=" self-center w-5 h-5 mr-2" />
+            {item.name}
           </a>
         );
       })}
